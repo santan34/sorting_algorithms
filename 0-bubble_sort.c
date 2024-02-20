082be5349475ee6f9237f2357b0f451afc07a1a7
St <takundaveni@gmail.com>
2024-02-20 14:37:44 -0800

    fixed some betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,7 @@
 void swap(int *one, int *two);
 /**
  * bubble_sort - The bubble sort algorithm
- * @array: the array to sort 
+ * @array: the array to sort
  * @size: the size of the array
  */
 void bubble_sort(int *array, size_t size)
@@ -10,6 +10,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0;
 	size_t j = 0;
 
+	if (!array || !size)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < (size - 1); j++)
@@ -29,9 +31,9 @@ void bubble_sort(int *array, size_t size)
  */
 void swap(int *one, int *two)
 {
-        int temp = 0;
+	int temp = 0;
 
-        temp = *one;
-        *one = *two;
-        *two = temp;
+	temp = *one;
+	*one = *two;
+	*two = temp;
 }
